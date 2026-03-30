700a0e39fe069b219a64d8180ce8826852cb0c88
farzz4 <farida1zran2@gmail.com>
2026-03-30 22:53:26 +0200

    Rectification gestion de document

---
+++ MediaTekDocuments/model/GestionAbonnement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MediaTekDocuments.model
+{
+    /// <summary>
+    /// Classe utilitaire pour les calculs de dates
+    /// </summary>
+    public class GestionAbonnement
+    {
+        /// <summary>
+        /// Vérifie si une date de parution est comprise entre deux dates
+        /// </summary>
+        public static bool ParutionDansAbonnement(DateTime dateCommande, DateTime dateFinAbonnement, DateTime dateParution)
+        {
+            return dateParution >= dateCommande && dateParution <= dateFinAbonnement;
+        }
+    }
+}
---
+++ MediaTekDocuments/view/FrmAlerte.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using MediaTekDocuments.model;
+
+namespace MediaTekDocuments.view
+{
+    public partial class FrmAlerte : Form
+    {
+        private readonly BindingSource bdgAlerte = new BindingSource();
+
+        /// <summary>
+        /// Constructeur : reçoit la liste des abonnements expirant bientôt
+        /// </summary>
+        public FrmAlerte(List<Abonnement> lesAbonnements)
+        {
+            InitializeComponent();
+            RemplirListe(lesAbonnements);
+        }
+
+        /// <summary>
+        /// Remplit le datagrid avec la liste des abonnements
+        /// </summary>
+        private void RemplirListe(List<Abonnement> lesAbonnements)
+        {
+            bdgAlerte.DataSource = lesAbonnements;
+            dgvAlerte.DataSource = bdgAlerte;
+            dgvAlerte.Columns["Id"].Visible = false;
+            dgvAlerte.Columns["IdRevue"].Visible = false;
+            dgvAlerte.Columns["Montant"].Visible = false;
+            dgvAlerte.Columns["DateCommande"].Visible = false;
+            dgvAlerte.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        /// <summary>
+        /// Ferme la fenêtre
+        /// </summary>
+        private void btnAlerteOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
